80ec477d7d6da40013d1449bbe9a3f4eee4ad538
l1umenncs <.email@.com>
2025-06-04 20:56:59 +0300

    progression

---
+++ bin/brain-progression.js
@@ -0,0 +1,16 @@
+#!usr/bin/env node
+
+import { generationArithmeticProgression, replaceRandomWithDots } from "../src/helpers/helpers";
+import runGame from "../src/runGame.js";
+
+const generateProgressionQuestion = () => {
+    const arr = generationArithmeticProgression()
+    const [newArr, correctAnswer] = replaceRandomWithDots(arr)
+
+    const question = newArr.join(' ')
+
+    return [question, correctAnswer]
+}
+
+const description = 'what number is missing in the progression'
+runGame(description, generateProgressionQuestion)
